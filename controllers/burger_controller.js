ebd83ad94bba7d1456c1cfdef2ffff6d07706289
Mike Conry <mikeconry@Mikes-MacBook-Pro.local>
2019-03-03 13:13:33 -0500

    burger_controller.js

---
+++ controllers/burger_controller.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+//Import the model (burger.js) to use its database functions
+var burger = require('../models/burger');
+
+// Create all our routes and set up logix within those routes where required
+router.get('/', function(req, res){
+    burger.SelectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/', function(req, res){
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ],[
+        req.body.burger_name, req.body.devoured
+    ], function(){
+        res.redirect('/');
+    });
+});
+
+router.put('/:id', function(req, res){
+    var condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(){
+        res.redirect('/');
+    });
+});
+
+//Export routes for server.js to use
+module.exports = router;
